5368f01dd3b73833ba34a0d56662dc4465aec036
akrindev <akrinmin@gmail.com>
2021-07-16 08:35:04 +0700

    disable slide

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -78,11 +78,11 @@
                         </form>
                     </ul>
                 </li>
-                <li class="nav-item">
+                {{-- <li class="nav-item">
                     <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
                         <i class="fas fa-th-large"></i>
                     </a>
-                </li>
+                </li> --}}
             </ul>
         </nav>
         <!-- End Navbar -->
