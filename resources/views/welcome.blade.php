28e072b66fbe855f19ca4ce7cc3f22226d84bf6d
akrindev <akrinmin@gmail.com>
2021-07-19 22:56:25 +0700

    fix landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,10 +11,10 @@
 
 <!-- Section 1 -->
 <section class="relative w-full px-8 text-gray-700 bg-white body-font">
-    <div class="flex items-center justify-between py-5 mx-auto max-w-7xl">
+    <div class="flex flex-wrap items-center justify-between py-5 mx-auto max-w-7xl">
         <a href="#_" class="flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">PPDB Smedip</a>
 
-        <nav class="flex items-center justify-center h-full py-5 space-x-5 text-base">
+        <nav class="flex items-center justify-center h-full py-5 space-x-3 text-base">
             <a href="#_" class="relative font-medium leading-6 text-gray-500 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
                 <span class="block">Home</span>
                 <span class="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
@@ -28,7 +28,7 @@
                 </span>
             </a>
             <a href="#tempat-belajar" class="relative font-medium leading-6 text-gray-500 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
-                <span class="block">Tempat Belajar</span>
+                <span class="block">Pembelajaran</span>
                 <span class="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
                     <span x-show="hover" class="absolute inset-0 inline-block w-full h-1 h-full transform bg-gray-900" x-transition:enter="transition ease duration-200" x-transition:enter-start="scale-0" x-transition:enter-end="scale-100" x-transition:leave="transition ease-out duration-300" x-transition:leave-start="scale-100" x-transition:leave-end="scale-0" style="display: none;"></span>
                 </span>
