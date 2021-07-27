1bb9522b26c5e116e2eabae0c7fe256a0854de95
akrindev <akrinmin@gmail.com>
2021-07-27 11:36:01 +0700

    fixes export peserta ppdb

--- app/Exports/PesertaPPDBExport.php
+++ app/Exports/PesertaPPDBExport.php
@@ -33,7 +33,31 @@ class PesertaPPDBExport implements FromCollection, WithHeadings, WithMapping
     {
         return [
             'No. Pendaftaran',
-            'Nama Lengkap'
+            'Nama Lengkap',
+            'Jenis Kelamin',
+            'Tempat Lahir',
+            'Tanggal Lahir',
+            'NIK',
+            'NISN',
+            'Alamat Lengkap',
+            'Asal Sekolah',
+            'Tahun Lulus',
+            'Penerima KIP',
+            'No. KIP',
+            'Nomor Telepon',
+            'Nama Ayah',
+            'Pekerjaan Ayah',
+            'Nomor Telepon Ayah',
+            'Nama Ibu',
+            'Pekerjaan Ibu',
+            'Nomor Telepon Ibu',
+            'Akademik Kelas / Semster / Peringkat',
+            'Akademik Hafidz / Hafidzoh',
+            'Non Akademik Jenis Lomba',
+            'Non Akademik Juara ke',
+            'Non Akademik Tingkat',
+            'Rekomendasi MWC',
+            'Saran Dari'
         ];
     }
 
@@ -42,7 +66,31 @@ class PesertaPPDBExport implements FromCollection, WithHeadings, WithMapping
     {
         return [
             $peserta->no_pendaftaran,
-            $peserta->nama_lengkap
+            $peserta->nama_lengkap,
+            $peserta->jenis_kelamin == 'l' ? 'Laki-laki' : 'Perempuan',
+            $peserta->tempat_lahir,
+            $peserta->tanggal_lahir->format('d F Y'),
+            $peserta->nik,
+            $peserta->nisn,
+            $peserta->alamat_lengkap,
+            $peserta->asal_sekolah,
+            $peserta->tahun_lulus,
+            $peserta->penerima_kip == 'y' ? 'Ya' : 'Tidak',
+            $peserta->no_kip,
+            $peserta->no_hp,
+            $peserta->nama_ayah,
+            $peserta->pekerjaan_ayah,
+            $peserta->no_hp_ayah,
+            $peserta->nama_ibu,
+            $peserta->pekerjaan_ibu,
+            $peserta->no_hp_ibu,
+            $peserta->akademik['kelas'] . ' / ' . $peserta->akademik['semester'] . ' / ' . $peserta->akademik['peringkat'],
+            $peserta->akademik['hafidz'],
+            $peserta->non_akademik['jenis_lomba'],
+            $peserta->non_akademik['juara_ke'],
+            $peserta->non_akademik['juara_tingkat'],
+            $peserta->rekomendasi_mwc == 1 ? 'Ya' : 'Tidak',
+            $peserta->saran_dari
         ];
     }
 }
