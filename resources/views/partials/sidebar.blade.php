70f7c118aa164ced940475b18b5ccaba5caafbeb
akrindev <akrinmin@gmail.com>
2021-08-02 08:56:59 +0700

    fixed link

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="/master/home" class="brand-link">
+    <a href="/" class="brand-link">
         <img src="/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">PPDB | Admin</span>
