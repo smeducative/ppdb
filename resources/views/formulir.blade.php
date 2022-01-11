bb2c0315f8e3b4b8880422899009136aba4749bc
akrindev <akrinmin@gmail.com>
2022-01-11 08:20:43 +0700

    revisi

--- resources/views/formulir.blade.php
+++ resources/views/formulir.blade.php
@@ -380,8 +380,8 @@
                     <div class="col-span-6 sm:col-span-3">
                         <label class="block text-sm font-medium text-gray-700">Tingkat</label>
                         <select class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" id="jtingkat" style="width: 100%;" name="juara_tingkat">
+                            <option value="">-- pilih tingkat --</option>
                             @foreach (['kabupaten/kota', 'provinsi', 'nasional'] as $tingkat)
-
                             <option value="{{ $tingkat }}">{{ $tingkat }}</option>
                             @endforeach
                         </select>
