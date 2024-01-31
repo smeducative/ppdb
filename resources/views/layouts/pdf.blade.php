692f2d7fdfd3c08db5e419f08993c0269e0590bb
Syakirin Amin <akrinmin@gmail.com>
2024-01-31 11:24:36 +0700

    fix: printing
    Signed-off-by: Syakirin Amin <akrinmin@gmail.com>

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/css-admin/adminlte.min.css') }}">
-	<style>
-		.page-break {
-    		page-break-after: always;
-		}
-	</style>
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+
+        @page {
+            size: B4;
+        }
+    </style>
 </head>
+
 <body>
 
 
     @yield('content')
 
     <script>
-
-  window.addEventListener("load", window.print());
+        window.addEventListener("load", window.print());
     </script>
 </body>
 </html>
