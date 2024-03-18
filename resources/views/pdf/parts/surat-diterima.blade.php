f7a2e6aab6cb0a5cd6f1b12d3a3ef55cbe6814e1
Syakirin Amin <akrinmin@gmail.com>
2024-03-18 09:34:07 +0700

    fix: font size
    Signed-off-by: Syakirin Amin <akrinmin@gmail.com>

--- resources/views/pdf/parts/surat-diterima.blade.php
+++ resources/views/pdf/parts/surat-diterima.blade.php
@@ -23,7 +23,7 @@
     <!-- hr -->
     <hr style="border: 2px solid black;" class="my-0">
 
-    <table style="margin-top: 18px; font-size: 19px;" width="55%">
+    <table style="margin-top: 18px; font-size: 21px;" width="55%">
         <tr>
             <td width="13%">No</td>
             <td>:</td>
@@ -43,7 +43,7 @@
         </tr>
     </table>
 
-    <div class="row mb-5">
+    <div class="row mb-5" style="font-size: 21px">
         <div class="col-4">&nbsp;</div>
         <div class="col-4">&nbsp;</div>
         <div class="col-4" style="font-size: 19px;">
@@ -54,68 +54,71 @@
         </div>
     </div>
 
-<strong style="font-size: 18px;" class="d-block">Assalamu’alaikum Wr. Wb.</strong>
-<p style="font-size: 18px;">
-    Teriring puja dan puji syukur kehadirat Allah SWT serta Sholawat salam teruntuk Baginda Rasulullah SAW. Bersama ini kami
-    sampaikan bahwa, putra/putri Bapak/Ibu yang tersebut di bawah ini
-</p>
+<div style="font-size: 20px">
 
-<table style="margin-left: 60px; font-size: 18px;">
-    <tr>
-        <td width="30%">Nama</td>
-        <td>:</td>
-        <td> &nbsp; {{ $peserta->nama_lengkap }}</td>
-    </tr>
-    <tr>
-        <td width="30%">Nomor Pendaftaran</td>
-        <td>:</td>
-        <td> &nbsp; {{ $peserta->no_pendaftaran }}</td>
-    </tr>
-    <tr>
-        <td width="30%">Alamat</td>
-        <td>:</td>
-        <td> &nbsp; {{ $peserta->alamat_lengkap }}</td>
-    </tr>
-    <tr>
-        <td width="30%">Asal Sekolah</td>
-        <td>:</td>
-        <td> &nbsp; {{ $peserta->asal_sekolah }}</td>
-    </tr>
-</table>
+    <strong style="font-size: 18px;" class="d-block">Assalamu’alaikum Wr. Wb.</strong>
+    <p style="font-size: 18px;">
+        Teriring puja dan puji syukur kehadirat Allah SWT serta Sholawat salam teruntuk Baginda Rasulullah SAW. Bersama ini kami
+        sampaikan bahwa, putra/putri Bapak/Ibu yang tersebut di bawah ini
+    </p>
 
-<p style="margin-top: 18px;">
-    Berdasarkan hasil ujian seleksi Penerimaan Peserta Didik Baru SMK Diponegoro TA. {{ now()->year }}/{{ now()->addYear()->year }} dinyatakan :
-</p>
-<div class="text-center" style="font-size: 20px; border: 2px solid black; padding: 14px;">
-    <strong class="d-block">
-        LULUS/<strike>TIDAK LULUS</strike> dan DITERIMA/<strike>TIDAK</strike>
-    </strong>
-    <small class="d-block">Pada Program Keahlian:</small>
-    <strong class="d-block">{{ $peserta->jurusan->nama }} ({{ $peserta->jurusan->abbreviation }})</strong>
+    <table style="margin-left: 60px; font-size: 18px;">
+        <tr>
+            <td width="30%">Nama</td>
+            <td>:</td>
+            <td> &nbsp; {{ $peserta->nama_lengkap }}</td>
+        </tr>
+        <tr>
+            <td width="30%">Nomor Pendaftaran</td>
+            <td>:</td>
+            <td> &nbsp; {{ $peserta->no_pendaftaran }}</td>
+        </tr>
+        <tr>
+            <td width="30%">Alamat</td>
+            <td>:</td>
+            <td> &nbsp; {{ $peserta->alamat_lengkap }}</td>
+        </tr>
+        <tr>
+            <td width="30%">Asal Sekolah</td>
+            <td>:</td>
+            <td> &nbsp; {{ $peserta->asal_sekolah }}</td>
+        </tr>
+    </table>
+
+    <p style="margin-top: 18px;">
+        Berdasarkan hasil ujian seleksi Penerimaan Peserta Didik Baru SMK Diponegoro TA. {{ now()->year }}/{{ now()->addYear()->year }} dinyatakan :
+    </p>
+    <div class="text-center" style="font-size: 20px; border: 2px solid black; padding: 14px;">
+        <strong class="d-block">
+            LULUS/<strike>TIDAK LULUS</strike> dan DITERIMA/<strike>TIDAK</strike>
+        </strong>
+        <small class="d-block">Pada Program Keahlian:</small>
+        <strong class="d-block">{{ $peserta->jurusan->nama }} ({{ $peserta->jurusan->abbreviation }})</strong>
+    </div>
+
+    <div class="mt-2" style="font-size: 18px;">
+        Sehubungan dengan terbatas kelas yang ada, maka Bapak/Ibu/Saudara dapat segera langsung melaksanakan <strong>registrasi daftar
+        ulang setelah dinyatakan diterima.</strong> <br>
+        <strong>
+            Apabila sebelum batas akhir yang telah ditentukan ({{ $carbon->parse($setting->body['batas_akhir_ppdb'])->translatedFormat('d F Y') }})
+        </strong>, kelas/rombel yang tersedia telah terpenuhi quotanya
+        (jumlah peserta didik yang diterima), maka dengan berat hati kami sampaikan Putra/Putri Bapak/Ibu <strong>tidak dapat diterima</strong>
+        di kelas tersebut. <br>
+        Biaya registrasi yang telah dibayarkan ke SMK Diponegoro Karanganyar <strong>tidak dapat diambil</strong> kembali apabila Putra/Putri
+        Bapak/Ibu diterima di SMA/SMK lain dengan syarat dan ketentuan yang telah ditetapkan sekolah. Adapun besaran biaya
+        registrasi terlampir.
+        <br><br>
+        Demikian surat pemberitahuan ini disampaikan. Atas perhatian dan kerjasamanya disampaikan terima kasih.
+        <br>
+        <strong>
+
+            Wassalamu’alaikum Wr. Wb.
+        </strong>
+
+    </div>
 </div>
 
-<div class="mt-2" style="font-size: 18px;">
-    Sehubungan dengan terbatas kelas yang ada, maka Bapak/Ibu/Saudara dapat segera langsung melaksanakan <strong>registrasi daftar
-    ulang setelah dinyatakan diterima.</strong> <br>
-    <strong>
-        Apabila sebelum batas akhir yang telah ditentukan ({{ $carbon->parse($setting->body['batas_akhir_ppdb'])->translatedFormat('d F Y') }})
-    </strong>, kelas/rombel yang tersedia telah terpenuhi quotanya
-    (jumlah peserta didik yang diterima), maka dengan berat hati kami sampaikan Putra/Putri Bapak/Ibu <strong>tidak dapat diterima</strong>
-    di kelas tersebut. <br>
-    Biaya registrasi yang telah dibayarkan ke SMK Diponegoro Karanganyar <strong>tidak dapat diambil</strong> kembali apabila Putra/Putri
-    Bapak/Ibu diterima di SMA/SMK lain dengan syarat dan ketentuan yang telah ditetapkan sekolah. Adapun besaran biaya
-    registrasi terlampir.
-    <br><br>
-    Demikian surat pemberitahuan ini disampaikan. Atas perhatian dan kerjasamanya disampaikan terima kasih.
-    <br>
-    <strong>
-
-        Wassalamu’alaikum Wr. Wb.
-    </strong>
-
-</div>
-
-<div class="row mt-2">
+<div class="row mt-5" style="font-size: 20px">
     <div class="col-6">&nbsp;</div>
     <div class="col-6 text-center">
         <strong class="d-block">Karanganyar, {{ now()->translatedFormat('d F Y') }}</strong>
@@ -128,7 +131,7 @@
         ( {{ auth()->user()->name }} )
     </div>
 
-    <div class="col-12 mt-5">
+    <div class="col-12 mt-5" style="font-size: 16px">
         <strong>Nb:* Hari {{ $carbon->parse($setting->body['batas_akhir_ppdb'])->subDay()->translatedFormat('l. d F Y') }}</strong>  Pukul 07.00, Siswa berangkat memakai seragam Osis SMP/MTs untuk <strong>persiapan/pembekalan
         kegiatan Makesta.</strong>
     </div>
