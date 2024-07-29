06d691bbd991ce6e20d3a254f6fec55d1a3c9e6e
Syakirin Amin <akrinmin@gmail.com>
2024-07-29 08:45:55 +0700

    fix(admin): caching the years_visible

--- app/View/Composers/YearVisibleComposer.php
+++ app/View/Composers/YearVisibleComposer.php
@@ -4,6 +4,7 @@ namespace App\View\Composers;
 
 use App\Models\PesertaPPDB;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class YearVisibleComposer
@@ -11,16 +12,16 @@ class YearVisibleComposer
     /**
      * Create a new profile composer.
      */
-    public function __construct(
-        protected PesertaPPDB $users,
-    ) {}
+    public function __construct(protected PesertaPPDB $users)
+    {
+    }
 
     /**
      * Bind data to the view.
      */
     public function compose(View $view): void
     {
-        $oldestYear = $this->users->oldest('created_at')->first()->created_at->year;
+        $oldestYear = Cache::remember('oldest_year', 60 * 24 * 28, fn() => $this->users->oldest('created_at')->first()->created_at->year);
 
         $view->with('years_visible', $oldestYear);
     }
